a7a916d7ff18f906a603121319c7796204ec8b1d
jixinya <34118623+jixinya@users.noreply.github.com>
2024-07-15 10:49:18 +0200

    sh script

---
+++ preprocess/caption_objaverse.sh
@@ -0,0 +1,16 @@
+OUTPUT_ROOT=/media/yuanxun/G/dataset_captions
+WORK_ROOT=/media/yuanxun/G/dataset_captions_work_root
+
+python caption_objaverse.py  \
+    --video_root $VIDEO_ROOT/video_highquality_test \
+    --meta_root $META_ROOT \
+    --work_root $WORK_ROOT \
+    --bucket objaversexxxx \
+    --download_tar 1 \
+    --skip_tar 0 \
+    --num_work_tar 16 \
+    --load-8bit 0 \
+    --load-4bit 0 \
+    --process 16 \
+    --global_input_image_num 16 \
+    --num_global_prompts 1 \
